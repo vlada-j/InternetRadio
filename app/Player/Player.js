4792b160cf5f85d2948840ddef1f6e944971854f
Vlada Janosevic <vladajanosevic@gmail.com>
2015-07-12 01:12:54 +0200

    Player directive

--- app/Player/Player.js
+++ app/Player/Player.js
@@ -15,7 +15,8 @@
 angular
 .module ('InternetRadio.Player', [])
 .config (config)
-.run (run);
+.run (run)
+.directive ('irPlayer', irPlayer);
 
 
 //--------------------------------------------------------------------------------------------------
@@ -26,5 +27,48 @@ function config() {}
 //--------------------------------------------------------------------------------------------------
 run.$inject = [];
 function run() {}
+
+
+//--------------------------------------------------------------------------------------------------
+irPlayer.$inject = [];
+function irPlayer() {
+
+	return {
+		scope: {},
+		replace: true,
+		restrict: 'EAC',
+		templateUrl: 'app/Player/irPlayer.tpl.html',
+		link: link
+	};
+
+
+	function link(scope, ele) {
+		var audio = ele.find('audio'),
+			paused;
+
+		paused = false;
+		scope.toggleClass = 'zxc';
+		scope.volume = audio[0].volume * 100;
+		scope.toggle = toggle;
+		scope.$watch('volume', function(n) {
+			audio[0].volume = n / 100;
+		});
+
+		scope.title = 'zxc';
+		scope.tags = [];
+
+		function toggle() {
+			if (audio[0].paused) {
+				audio[0].play();
+				paused = false;
+				scope.toggleClass = 'pause-state';
+			} else {
+				audio[0].pause();
+				paused = true;
+				scope.toggleClass = 'play-state';
+			}
+		}
+	}
+}
 
 })();
