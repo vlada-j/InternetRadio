4be421c44fdb62890e1da3a5b0106c7ad9c8f0f0
Vlada Janosevic <vladajanosevic@gmail.com>
2015-07-11 15:09:05 +0200

    Vendors and  init

---
+++ app/app.js
@@ -0,0 +1,19 @@
+(function(){
+'use strict';
+
+angular
+.module('InternetRadio', [])
+.config(appConfig)
+.run(appRun);
+
+
+//--------------------------------------------------------------------------------------------------
+appConfig.$inject = [];
+function appConfig() {}
+
+
+//--------------------------------------------------------------------------------------------------
+appRun.$inject = [];
+function appRun() {}
+
+})();
